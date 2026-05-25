0a32ba6378c64800827bc5704043c3382852090a
Samuel Adedeji <djsam2828@gmail.com>
2026-05-25 12:21:12 +0100

    Updated the register validation

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -10,7 +10,32 @@ const registerSchema = z
   .object({
     name: z.string().min(2, "Name must be at least 2 characters"),
     email: z.string().email("Email address required"),
-    password: z.string().min(6, "Password must be at least 6 characters"),
+    password: z
+      .string()
+      .min(8, "Password must be at least 8 characters")
+      .regex(
+        /^(?=.*[A-Z])/,
+        "Password must contain at least one uppercase letter"
+      )
+      .regex(
+        /^(?=.*[a-z])/,
+        "Password must contain at least one lowercase letter"
+      )
+      .regex(
+        /^(?=.*\d)/,
+        "Password must contain at least one number"
+      )
+      .regex(
+        /^(?=.*[@$!%*?&.#])/,
+        "Password must contain at least one special character"
+      )
+      .refine(
+        (value) => !/(.)\1{4,}/.test(value),
+        {
+          message:
+            "Password cannot contain repeated characters.",
+        }
+      ),
     confirmPassword: z.string(),
   })
   .refine((data) => data.password === data.confirmPassword, {
